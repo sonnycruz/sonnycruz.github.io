f1a37cecf52e97396f3682023d8cd79a67933691
Sonny Cruz Torres <sonnycruztorres@gmail.com>
2018-12-27 12:35:16 -0600

    Deploying changes

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,7 +33,7 @@ SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
-STATIC_PATHS = ['images', ]
+STATIC_PATHS = ['images', 'files']
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
